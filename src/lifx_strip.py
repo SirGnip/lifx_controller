4922d44b13a349516b2b3f45ec59abb6cfbc47dd
SirGnip <SirGnip@users.noreply.github.com>
2026-01-02 17:44:24 -0600

    Clean out unused code

--- src/lifx_strip.py
+++ src/lifx_strip.py
@@ -110,86 +110,6 @@ def set_power(power: bool):
         s.set_power(power)
 
 
-def cycle():
-    api, lights = _setup()
-    strip = [d for d in lights if d.supports_multizone()][0]
-    strip.set_power(True)
-    strip.set_zone_color(0, 23, [0, 65000, 65000, 3000], apply=True)
-    time.sleep(1)
-    strip.set_zone_color(0, 23, [0, 65000, 10000, 3000], apply=True)
-    time.sleep(1)
-    strip.set_zone_color(0, 23, [30000, 65000, 10000, 3000], apply=True)
-    time.sleep(1)
-    strip.set_zone_color(0, 23, [60000, 65000, 10000, 3000], apply=True)
-    time.sleep(1)
-    strip.set_power(False)
-
-
-def fade(zone_idx):
-    DLY = 0.005
-    api, lights = _setup()
-    strip = [d for d in lights if d.supports_multizone()][0]
-    strip.set_power(True)
-    for b in range(0, 65000, 50):
-        if b % 5000 == 0:
-            print(b)
-        strip.set_zone_color(zone_idx, zone_idx, [0, 65000, b, 3000], apply=True, rapid=True)
-        time.sleep(DLY)
-    for b in range(65000, 0, -50):
-        if b % 5000 == 0:
-            print(b)
-        strip.set_zone_color(zone_idx, zone_idx, [0, 65000, b, 3000], apply=True, rapid=True)
-        time.sleep(DLY)
-
-
-def fade2(zone_idx, color, delay):
-    c_dark = color.copy()
-    c_bright = color.copy()
-    c_dark[2] = 0
-    c_bright[2] = 65000
-    api, lights = _setup()
-    strip = [d for d in lights if d.supports_multizone()][0]
-    strip.set_power(True)
-    print('zero')
-    strip.set_zone_color(zone_idx, zone_idx, c_dark, apply=True)
-    # time.sleep(0.05)
-    print('start up')
-    strip.set_zone_color(zone_idx, zone_idx, c_bright, apply=True, duration=delay * 1000)
-    time.sleep(delay)
-    print('start down')
-    strip.set_zone_color(zone_idx, zone_idx, c_dark, apply=True, duration=delay * 1000)
-    time.sleep(delay)
-    print('done')
-
-
-def cycle_zone(zone_idx):
-    api, lights = _setup()
-    strip = [d for d in lights if d.supports_multizone()][0]
-    strip.set_power(True)
-    colors = [lifxlan.BLUE, lifxlan.GREEN, lifxlan.RED, lifxlan.YELLOW, lifxlan.WHITE, lifxlan.PURPLE, lifxlan.ORANGE,
-              lifxlan.CYAN]
-    for c in colors:
-        strip.set_zone_color(zone_idx, zone_idx, c, apply=True)
-        time.sleep(0.8)
-    strip.set_power(False)
-
-
-async def async_fade(strip, zone_idx, color, iterations, delay):
-    c_dark = color.copy()
-    c_bright = color.copy()
-    c_dark[2] = 0
-    c_bright[2] = 65000
-
-    strip.set_zone_color(zone_idx, zone_idx, c_dark, apply=True)
-
-    half_delay = delay / 2
-    for i in range(iterations):
-        strip.set_zone_color(zone_idx, zone_idx, c_bright, apply=True, duration=half_delay * 1000)
-        await asyncio.sleep(half_delay)
-        strip.set_zone_color(zone_idx, zone_idx, c_dark, apply=True, duration=half_delay * 1000)
-        await asyncio.sleep(half_delay)
-
-
 async def base_fade(strip, zone_idx, color, delay):
     half_delay = delay / 2
 
@@ -214,41 +134,12 @@ def light_iterator(count: int | None):
         for i in range(count):
             yield i
 
-async def async_fade_master(strip, zone_idx, colors, iterations, delay):
-    for _ in light_iterator(iterations):
-        for z in range(24):
-            c = random.choice(colors)
-            delay = random.uniform(2, 5)
-            await base_fade(strip, z, c, delay)
-
-
-async def async_main():
-    api, lights = _setup()
-    strip = [d for d in lights if d.supports_multizone()][0]
-    strip.set_power(True)
-    rainbow = [lifxlan.RED, lifxlan.ORANGE, lifxlan.YELLOW, lifxlan.GREEN, lifxlan.BLUE, lifxlan.PURPLE]
-    # tasks = [asyncio.create_task(async_fade(strip, idx, random.choice(rainbow), 9, random.uniform(2, 4))) for idx in range(24)]
-    cool = [lifxlan.WHITE, lifxlan.WHITE, lifxlan.WHITE, lifxlan.WHITE, lifxlan.BLUE]
-    tasks = [asyncio.create_task(async_fade(strip, idx, random.choice(cool), 5, random.uniform(5, 7))) for idx in range(24)]
-    await asyncio.gather(*tasks)
-    print('done')
-    strip.set_power(False)
 
 async def async_zone_fader(strip, zone_idx, iterations, delay_min, delay_max, colors):
     for _ in light_iterator(iterations):
         color = random.choice(colors)
         delay = random.uniform(delay_min, delay_max)
         await base_fade(strip, zone_idx, color, delay)
-
-async def async_main2():
-    api, lights = _setup()
-    strip = [d for d in lights if d.supports_multizone()][0]
-    strip.set_power(True)
-    tasks = [asyncio.create_task(async_zone_fader(strip, idx, 5)) for idx in range(24)]
-    print(f'created {len(tasks)} tasks')
-    await asyncio.gather(*tasks)
-    print('done')
-    strip.set_power(False)
 
 
 def _parse_color(color):
@@ -265,7 +156,7 @@ def _parse_color(color):
         return vals
 
 
-async def async_main_both(discover, iterations, fade_min, fade_max, colors):
+async def async_main(discover, iterations, fade_min, fade_max, colors):
     # logging
     logging.info(f'Starting with {len(colors)} colors: {[c for c in colors]}')
     logging.info(f'Fade duration: {fade_min}-{fade_max} seconds')
@@ -450,29 +341,17 @@ def cmd():
 
 
 if __name__ == '__main__':
+    # cmd()  # run command line interface
     # dump()
     # set_power(False)
     # set_power(True)
     # reset_white()
     # reset_dark()
     # reset_black()
-    # cycle()
-    # cycle_zone(5)
-    # cycle_zone(7)
-    # fade(15)
-    # fade2(16, lifxlan.BLUE, 3)
-    # fade2(5, lifxlan.ORANGE, 2)
-    # fade2(12, lifxlan.PURPLE, 4)
-    # cmd()  # run command line interface
 
-    # asyncio.run(async_main())
-    # asyncio.run(async_main2())
-    # asyncio.run(async_main_both())
-
-    # Command line interface
     args = parse_args()
     logging.info(f'Command line args: {args}')
-    asyncio.run(async_main_both(
+    asyncio.run(async_main(
         discover=args.discover,
         iterations=args.iterations,
         fade_min=args.fade_min,
